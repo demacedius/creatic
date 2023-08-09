034cdc05237f97acd7d28fcc6823907dd3c6e315
demacedius <demacedius@gmail.com>
2023-08-10 00:52:21 +0200

    empty

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,11 +5,11 @@
 <Navbar/>
 <section>
     <div class="w-full h-full bg-gradient-to-r from-blue0 from-750% to-blue67/[15%]">
-        <img src="src/assets/beautiful-woman-with-big-jewel.png" alt="beautiful-woman-with-big-jewel" class=" mix-blend-screen">
+        <img src="../src/assets/beautiful-woman-with-big-jewel.png" alt="beautiful-woman-with-big-jewel" class=" mix-blend-screen">
     </div>
 
     <div class="CTA">
-        
+
     </div>
 </section>
 
